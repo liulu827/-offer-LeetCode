89f1082e0d74d3e8a451db826f8ea81cba8a74f1
Liu Lulu <liululu827@163.com>
2020-02-12 21:56:34 +0800

    完善斐波那契数列代码，时间复杂度O（n）

--- 剑指offer/0-斐波那契数列问题/斐波那契数列.py
+++ 剑指offer/0-斐波那契数列问题/斐波那契数列.py
@@ -3,8 +3,19 @@
 n<=39 
 '''
 # -*- coding:utf-8 -*-
+# 时间复杂度O(n)，还可以使用递归，时间复杂度太大O(2^n)
 class Solution:
     def Fibonacci(self, n):
         # write code here
-        for i in
-        
+        # 0,1,1,2,3,5,8 f(n) = f(n-1)+f(n-2)
+        if n >= 0:
+            a = 0
+            b = 1
+            for _ in range(n):
+                a, b = b, a + b # 把每一步的数据都记录了起来
+            return a
+        return None
+
+result = Solution()
+for n in range(34):
+    print(result.Fibonacci(n))
